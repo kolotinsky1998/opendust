eb238b255575efa720ae5a04567c160cc8b2e0df
kolotinsky1998 <kolotinsky.daniil1998@gmail.com>
2022-02-26 00:28:53 +0300

    batch script correction

--- examples/Piel2017/Figure1/given/scriptPiel2017Figure1Given.sh
+++ examples/Piel2017/Figure1/given/scriptPiel2017Figure1Given.sh
@@ -2,12 +2,12 @@
 #SBATCH --job-name=opendustPiel2017Fig1Given     
 #SBATCH --error=errPiel2017Fig1Given             
 #SBATCH --output=outputPiel2017Fig1Given       
-#SBATCH --time=01:00:00            
+#SBATCH --time=24:00:00            
 #SBATCH --ntasks=1                
 #SBATCH --nodes=1                  
 #SBATCH --gpus=1                   
 #SBATCH --cpus-per-task=1         
-#SBATCH  --constraint="type_a|type_b|type_c|type_e"
+#SBATCH  --constraint="type_a|type_b|type_c"
 
 
 module load CUDA/11.4
